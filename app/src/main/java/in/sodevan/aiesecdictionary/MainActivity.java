3c024f6c35104d3164fa30e35eda86679a6dfc95
kartiksharma <98kartik.sharma@gmail.com>
2017-03-11 02:09:09 +0530

    final additions in the app(added hashmap,icons etc)

--- app/src/main/java/in/sodevan/aiesecdictionary/MainActivity.java
+++ app/src/main/java/in/sodevan/aiesecdictionary/MainActivity.java
@@ -15,10 +15,12 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    ArrayList<String> items,meaning;
+    ArrayList<String> items;
+    HashMap<String,String> meaning;
 
 
     @Override
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         items.add("AIESEC");
         items.add("AC");
         items.add("AI");
-        items.add("AP GN");
+        items.add("APGN");
         items.add("BOA");
         items.add("BUDDY");
         items.add("CEED");
@@ -42,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         items.add("DELEGATE");
         items.add("DI");
         items.add("DT");
-        items.add("ES");
+        items.add("EB");
         items.add("ELECT");
         items.add("ET");
         items.add("GCDP");
@@ -75,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
         items.add("OPS");
         items.add("PAI");
         items.add("P -BOX");
-        items.add("exchange PLENARY");
         items.add("REALIZED");
         items.add("ROLL CALL");
         items.add("RST");
@@ -88,67 +89,67 @@ public class MainActivity extends AppCompatActivity {
         items.add("TRAINEESHIP");
         items.add("WENA GN");
         items.add("Y2B");
-        meaning=new ArrayList<>();
-        meaning.add("Association internationale des etudiants en sciences economiques et commerciales");
-        meaning.add("Assessment Centre - case studies and individual tasks for students while going through selection process for membership.");
-        meaning.add("AIESEC International");
-        meaning.add("Asia Pacific Growth Network.");
-        meaning.add("Board of Advisors.");
-        meaning.add("A member who is the contact person responsible for the well-being of an intern during his or her stay in the country. ");
-        meaning.add("Exchange of AIESEC members between two LCs. AIESEC members can work fora period of time in a LC abroad doing AIESEC work. Usually takes from a week to 6 months. ");
-        meaning.add("Central and Eastern Europe Growth Network.");
-        meaning.add("Career Days. ");
-        meaning.add("Challenging Time.");
-        meaning.add("Session Challengin World View.");
-        meaning.add("AIESEC code for \"Country\"");
-        meaning.add("Participant in a conference or seminar. Follows the complete agenda. ");
-        meaning.add("Type of internship - Development Internship \n");
-        meaning.add("DELHI IIT ;)");
-        meaning.add("Executive Board");
-        meaning.add("Name for an executive board member from the time when he or she is elected and up to the time where they take over.");
-        meaning.add("Type of internship - Educational Internship. ");
-        meaning.add("Global Community Development Program.");
-        meaning.add("Growth Network -AIESEC countries are divided into 5 growth networks (WENA, SS, CEE, Africa and AP).");
-        meaning.add("Global Internship Program.");
-        meaning.add("Growth Network");
-        meaning.add("Global Exchange Partner");
-        meaning.add("International Congress.");
-        meaning.add("Initiative group");
-        meaning.add("Session Individual Learning Plan.");
-        meaning.add("Products provided for free as a sponsorship.");
-        meaning.add("Icelandic National Semina");
-        meaning.add("International President Meeting");
-        meaning.add("Exchange Quality Policies and Standards ");
-        meaning.add("Local Committee.");
-        meaning.add("Local Committee Executive Board. ");
-        meaning.add("Local Committee President. Head of the local executive board. ");
-        meaning.add("Local Committee Vice President.");
-        meaning.add("Leadership Development Seminar. ");
-        meaning.add("Local Education Cycle.");
-        meaning.add("Learning network");
-        meaning.add("Learning Week.");
-        meaning.add("Member Committee Executive Board.");
-        meaning.add("Member Committee President.");
-        meaning.add("Member Committee Vice President.");
-        meaning.add("Type of internship - Management internship");
-        meaning.add("Status for company or student in Insight XP. They found suitable match and now are in process of preparing everything for intern's arrival.");
-        meaning.add("Organizing Committee");
-        meaning.add("Organizing Committee President");
-        meaning.add("Outgoing Preparation Seminar. ");
-        meaning.add("President of AIESEC International.");
-        meaning.add("Project Based on Exchange.");
-        meaning.add("Status for company or student in Insight XP. Intern already arrived to country for internship and is doing his job in company.");
-        meaning.add("Dance");
-        meaning.add("Regional Support Team. ");
-        meaning.add("Student Nominee.");
-        meaning.add("Spanish Speaking Growth Network.");
-        meaning.add("Traineeship Nominee");
-        meaning.add("Co-operation between two LCs from different countries. Normally both LCs visit each other, have a seminar and create and work on strategies of cooperation.");
-        meaning.add("Type of traineeship - Technical Traineeship.");
-        meaning.add("Name for intern in receiving country.");
-        meaning.add("The same meaning as international internship. ");
-        meaning.add("Westem Europe and Northern America Growth Network. ");
-        meaning.add("Youth to Business. Leadership Seminar. ");
+        meaning=new HashMap<String, String>();
+        meaning.put("AIESEC","Association internationale des etudiants en sciences economiques et commerciales");
+        meaning.put("AC","Assessment Centre - case studies and individual tasks for students while going through selection process for membership.");
+        meaning.put("AI","AIESEC International");
+        meaning.put("APGN","Asia Pacific Growth Network.");
+        meaning.put("BOA","Board of Advisors.");
+        meaning.put("BUDDY","A member who is the contact person responsible for the well-being of an intern during his or her stay in the country. ");
+        meaning.put("CEED","Exchange of AIESEC members between two LCs. AIESEC members can work fora period of time in a LC abroad doing AIESEC work. Usually takes from a week to 6 months. ");
+        meaning.put("CEE GN","Central and Eastern Europe Growth Network.");
+        meaning.put("CDs","Career Days. ");
+        meaning.put("CT","Challenging Time.");
+        meaning.put("CVVV","Session Challengin World View.");
+        meaning.put("CY","AIESEC code for Country");
+        meaning.put("DELEGATE","Participant in a conference or seminar. Follows the complete agenda. ");
+        meaning.put("DT","Type of internship - Development Internship ");
+        meaning.put("DI","DELHI IIT ;)");
+        meaning.put("EB","Executive Board");
+        meaning.put("ELECT","Name for an executive board member from the time when he or she is elected and up to the time where they take over.");
+        meaning.put("ET","Type of internship - Educational Internship. ");
+        meaning.put("GCDP","Global Community Development Program.");
+        meaning.put("GGN","Growth Network -AIESEC countries are divided into 5 growth networks (WENA, SS, CEE, Africa and AP).");
+        meaning.put("GIP","Global Internship Program.");
+        meaning.put("GN","Growth Network");
+        meaning.put("GEP","Global Exchange Partner");
+        meaning.put("IC","International Congress.");
+        meaning.put("IG","Initiative group");
+        meaning.put("ILP","Session Individual Learning Plan.");
+        meaning.put("INKIND","Products provided for free as a sponsorship.");
+        meaning.put("INS","Icelandic National Semina");
+        meaning.put("IPM","International President Meeting");
+        meaning.put("IXQPS","Exchange Quality Policies and Standards ");
+        meaning.put("LC","Local Committee.");
+        meaning.put("LCEB","Local Committee Executive Board. ");
+        meaning.put("LCP","Local Committee President. Head of the local executive board. ");
+        meaning.put("LCVP","Local Committee Vice President.");
+        meaning.put("LDS","Leadership Development Seminar. ");
+        meaning.put("LEC","Local Education Cycle.");
+        meaning.put("LN","Learning network");
+        meaning.put("LW","Learning Week.");
+        meaning.put("MCEB","Member Committee Executive Board.");
+        meaning.put("MCP","Member Committee President.");
+        meaning.put("MCVP","Member Committee Vice President.");
+        meaning.put("MT","Type of internship - Management internship");
+        meaning.put("MATCHED","Status for company or student in Insight XP. They found suitable match and now are in process of preparing everything for intern's arrival.");
+        meaning.put("OC","Organizing Committee");
+        meaning.put("OCP","Organizing Committee President");
+        meaning.put("OPS","Outgoing Preparation Seminar. ");
+        meaning.put("PAI","President of AIESEC International.");
+        meaning.put("P -BOX","Project Based on Exchange.");
+        meaning.put("REALIZED","Status for company or student in Insight XP. Intern already arrived to country for internship and is doing his job in company.");
+        meaning.put("ROLL CALL","Dance");
+        meaning.put("RST","Regional Support Team. ");
+        meaning.put("SN","Student Nominee.");
+        meaning.put("SSGN","Spanish Speaking Growth Network.");
+        meaning.put("TN","Traineeship Nominee");
+        meaning.put("TWIN","Co-operation between two LCs from different countries. Normally both LCs visit each other, have a seminar and create and work on strategies of cooperation.");
+        meaning.put("TT","Type of traineeship - Technical Traineeship.");
+        meaning.put("TRAINEE","Name for intern in receiving country.");
+        meaning.put("TRAINEESHIP","The same meaning as international internship. ");
+        meaning.put("WENA GN","Westem Europe and Northern America Growth Network. ");
+        meaning.put("Y2B","Youth to Business. Leadership Seminar. ");
 
 
 
@@ -163,9 +164,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
              //   int posi= (int) listView.getItemAtPosition(position);
-                String a= meaning.get(position);
-                Log.d("TAG",a);
-                Toast.makeText(MainActivity.this,a,Toast.LENGTH_LONG);
+                String text=listView.getItemAtPosition(position).toString();
+                String mean=meaning.get(text);
+                Log.d("TAG",mean);
+                Toast.makeText(MainActivity.this,""+mean,Toast.LENGTH_LONG).show();
             }
         });
     }
